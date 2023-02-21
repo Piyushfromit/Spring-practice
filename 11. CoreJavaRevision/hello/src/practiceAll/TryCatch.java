44b0b5a57b49b0a3e2bd8ae09d58a6ef41e371dc
piyushfromit <piyushfrommasai@gmail.com>
2023-02-21 22:01:43 +0530

    a

---
+++ 11. CoreJavaRevision/hello/src/practiceAll/TryCatch.java
@@ -0,0 +1,25 @@
+package practiceAll;
+
+public class TryCatch {
+	
+	public static void main(String[] args) {
+		int n = 10;
+		
+		int m = 0;
+		
+		try {
+			
+			System.out.println(n/m);
+			
+		} catch (Exception e) {
+			
+			System.out.println( "number devide by 0 is not possible");
+			
+		}
+		
+		
+	}
+	
+	
+
+}
