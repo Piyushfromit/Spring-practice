50c3fdf8b00b8f57529f25e31b0756b9119241dc
piyushfromit <piyushfrommasai@gmail.com>
2023-02-22 23:54:30 +0530

    a

---
+++ 11. CoreJavaRevision/hello/src/practiceAll/ExceptionUsingThrow.java
@@ -0,0 +1,30 @@
+package practiceAll;
+
+import java.io.IOException;
+
+public class ExceptionUsingThrow {
+	
+	  public static void main(String args[]){  
+	    
+		try{  
+	       M m=new M();  
+	       m.method();  
+	    }catch(Exception e){
+	    	System.out.println("exception handled");
+	    }     
+	  
+	    System.out.println("normal flow...");  
+	  }  
+	
+	
+	
+
+}
+
+
+class M{  
+	 
+	void method()throws IOException{  
+	  throw new IOException("device error");  
+	 }  
+}  
