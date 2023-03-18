60ab3544cc99e62381c0c3899502fbd3d15805aa
piyushfromit <piyushfrommasai@gmail.com>
2023-03-18 10:45:44 +0530

    a

--- 11. CoreJavaRevision/hello/src/ArrayIntQuestion/SelectionSort.java
+++ 11. CoreJavaRevision/hello/src/ArrayIntQuestion/SelectionSort.java
@@ -11,7 +11,7 @@ public class SelectionSort {
 			int minInd = i;
 			
 			for( int j=i+1; j< arr.length; j++) {
-				if(arr[j] < arr[minInd] ) {
+				if(arr[minInd] > arr[j]  ) {
 					minInd=j;
 				}
 			}
